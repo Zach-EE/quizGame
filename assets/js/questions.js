53223b8b56e5d58fd943e3195fdf55cd17d79110
Zach Holloway <zachholloway.n@gmail.com>
2021-07-15 13:52:18 -0400

    added content to question arrays in questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,73 @@
+//*Array containing question number, question, answer, and potential answers
+
+let questions = [
+    {
+        number: 1,
+        question: "Commonly used data types DO NOT include:",
+        answer: "booleans",
+        choices: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ]
+    },
+    {
+        number: 2,
+        question: "The condition in an if/ else statement is enclosed within ___.",
+        answer: "",
+        choices: [
+            "quotes",
+            "curly brackets",
+            "parentheses",
+            "square brackets"
+        ]
+    },
+    {
+        number: 3,
+        question: "Arrays in JavaScript can be used to store ___.",
+        answer: "all of the above",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ]
+    },
+    {
+        number: 4,
+        question: "String values must be enclosed within ___ when being assigned to variables.",
+        answer: "quotes",
+        choices: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parentheses"
+        ]
+    },
+    {
+        number: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        answer: "console.log",
+        choices: [
+            "JavaScript",
+            "terminal/ bash",
+            "for loops",
+            "console log"
+        ]
+    }
+    /* 
+    TODO: Additional question template
+        {
+            number: n,
+            question: "question text",
+            answer: "correct answer text",
+            choices: [
+                "answer text",
+                "answer text",
+                "answer text",
+                "answer text"
+            ]
+        }
+    */   
+]
